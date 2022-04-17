49f793fc2d5c947089fa677ead6674b3a3ff37d2
corina nattero <corinattero@gmail.com>
2022-04-17 19:33:19 -0300

    se agrega el proyecto

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* todo lo que esta dentro del header se puede eliminar y trabajar dentro */}
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1> HOLA CORINA Bienvenida a React</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
