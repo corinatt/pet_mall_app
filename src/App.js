e30b3602c2204e1ca12cbffeeed51644e18059b7
corina nattero <corinattero@gmail.com>
2022-04-18 00:01:47 -0300

    se actualiza el Navbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 
 import './App.css';
+import DaisyNavBar from './Components/DaisyNavBar';
 import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <header className="App-header">
-        <h1 className='text-3xl font-bolde underline'> BIENVENIDOS</h1>
-        {/* todo lo que esta dentro del header se puede eliminar y trabajar dentro 
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>*/}
-      </header>
+      <div className='bg-gradient-to-r from-indigo-500'/>
+
+      <DaisyNavBar/>
+      {/*<NavBar/>*/}
+    
     </div>
   );
 }
